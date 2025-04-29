bb9a0655fd3122874098b0f85eed0a4b44ae72e4
vedanthanekar45 <vedanthanekar45@gmail.com>
2025-04-29 11:23:32 +0530

    chore: books and author models added

--- lending/models.py
+++ lending/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+# The class Author is to create a ManyToMany Field in the books model.
+# This is created because a single book can have multiple authors.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+
+# The books model is created to have a record of the books the library has. IF the library already uses LMS
+# like Koha or FOLIO, their data may be referenced through an API. Smaller libraries with no LMS may use 
+# this model.
+
+class BooksModel (models.Model):
+    title = models.CharField(max_length=255)
+    authors = models.ManyToManyField(Author, related_name='books')
+    ISBN = models.BigIntegerField()
+    Publisher = models.CharField(max_length=255)
+    no_of_copies = models.IntegerField(default=0)
+    status = models.CharField(choices = [
+        ("available", "Available"),
+        ("checked_out", "Checked Out"),
+        ("missing", "Missing"),
+        ("damaged", "Damaged")
+    ])
+    additional_tags = models.CharField(default="N/A")
+
+    def __str__ (self):
+        return self.title
